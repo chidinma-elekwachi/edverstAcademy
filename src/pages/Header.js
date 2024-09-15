1cfeee1c8ca4eed5021de253a91aae70a427cee6
chidinma-elekwachi <elexisbiz@gmail.com>
2024-09-15 18:19:04 +0100

    nav bar woeking well

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo1 from '../Assets/edverest.png';
-import '../styles/Header.css'
+import '../styles/Header.css';
 
-const Header = () => (
-  <header className="header">
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    <nav id='myNav'>
-      <a href='/'><img src={logo1} alt='edverest' id='logonav'></img></a>
-      
-      <div className='inWbtn'>
-        <input className='myInput' type='text' placeholder='Search for Anything'></input>
-        <button className='secondBtn search'><a href='/' id='signUpA'>Search</a></button>
-      </div>
-      
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-      <div className='linkSec'>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/courses">Courses</Link>
-        <Link to="/contact">Contact</Link>
-      </div>
+  return (
+    <header className="header">
+      <nav id='myNav'>
+        <a href='/'><img src={logo1} alt='edverest' id='logonav' /></a>
 
-      <div className='navButtons'>
+        <div className='inWbtn'>
+          <input className='myInput' type='text' placeholder='Search for Anything' />
+          <button className='secondBtn search'><a href='/' id='signUpA'>Search</a></button>
+        </div>
+
+        <div className={`linkSec ${menuOpen ? 'active' : ''}`}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/contact">Contact</Link>
+          {/* Move Sign In and Sign Up buttons here */}
           <button id='firstBtn'><Link to="/signIn">Sign In</Link></button>
           <button className='secondBtn'><Link to="signUp" id='linkTxt'>Sign up</Link></button>
+        </div>
 
-
-      </div>
-    </nav>
-  </header>
-);
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
