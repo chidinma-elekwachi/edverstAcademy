26864aa0093f5629b7f739d0a0b51a79849ad4ff
chidinma-elekwachi <elexisbiz@gmail.com>
2025-01-02 14:38:45 +0100

    removing sliding effect

--- src/components/Home.js
+++ src/components/Home.js
@@ -122,28 +122,10 @@ function Home() {
     }
   ];
 
-  
-  const boxes = document.querySelectorAll('.slideIn');
-  window.addEventListener('scroll', checkBoxes);
-  checkBoxes()
-
-  function checkBoxes() {
-    const triggerBottom = window.innerHeight/ 5 * 4
-     boxes.forEach(box => {
-      const boxTop = box.getBoundingClientRect().top
-
-      if (boxTop < triggerBottom) {
-        box.classList.add('show')
-      } else {
-        box.classList.remove('show')
-      }
-     
-    })
-  }
 
   return (
     <div>
-      <main className='homePage slideIn'>
+      <main className='homePage'>
         <div className='introText'>
           <h1 id='firstH1' className='bigBlue'>Explore Over <span>100's</span> of our courses to kick start your career.</h1>
           <p id='firstP'>Engage with interactive courses, expert instructors, and a community of learners to further your career.</p>
@@ -154,7 +136,7 @@ function Home() {
         </div>
       </main>
 
-      <div className='secondSec slideIn'>
+      <div className='secondSec'>
         <div className='sndTxt'>
           <h1 className='bigBlue'>Start Your Journey With Us</h1>
           <p>
@@ -168,14 +150,14 @@ function Home() {
         </section>
       </div>
 
-      <div className='thirdSec slideIn'>
+      <div className='thirdSec'>
             <div className='textSec'>
               <h1 id='myH1'>Join More than <span>1,250+</span> Learners around the world.</h1>
               <button id='courseBtn'><a href='./Courses' id='diveTxt'>See All Courses</a></button>
             </div>
       </div>
 
-      <div className='courseCat slideIn'>
+      <div className='courseCat'>
           <div className='catHead'>
               <h1>Our Popular Courses by Category</h1>
           </div>
@@ -189,7 +171,7 @@ function Home() {
           </center>
       </div>
           
-        <div className='slideIn'>
+        <div className=''>
           <div className='catHead'>
               <h1>Meet Our Instructors</h1>
           </div>
